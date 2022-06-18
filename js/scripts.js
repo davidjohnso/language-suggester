944172b258fc97bdde2aa336d0cf076c8f2f5eaa
David Johnson <djohnso08@gmail.com>
2022-06-18 15:26:52 -0500

    change non working code back to working

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,8 @@ function leaveButtonAlert(event) {
   alert("Where do you think your going! You can't leave yet!");
 };
 
-function invalidResponses(event) {
-  event.preventDefault();
-  alert("Please input valid responses");
-}
-
 //User Interface Logic
-window.onload = function() {
   window.addEventListener("load", function() {
-    const button = document.getElementById("button-two");
-    button.addEventListener("click",leaveButtonAlert);
+    const form = document.getElementById("button-two");
+    form.addEventListener("click", leaveButtonAlert);
   });
-};
