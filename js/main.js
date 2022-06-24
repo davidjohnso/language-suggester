83af5d2a75ff7da0cb75cfdacb2e17ccf667e89e
David Johnson <djohnso08@gmail.com>
2022-06-24 10:51:37 -0500

    add leave alert function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-window.onload = function() { 
-  if(button-two) {
-    let leaveButton = document.getElementById("button-two");
-    leaveButton.addEventListener("click", leaveAlert);
-  }
-}
+//Business Logic
+function leaveAlert() {
+  alert("Where do you think your going! You can't leave yet!");
+};
+
+// User Interface Logic
+window.onload = function() {
+  let leaveButton = document.getElementById("button-two");
+  leaveButton.addEventListener("click", leaveAlert);
+};
