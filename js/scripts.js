fee0ecc0a09b22d639007409e993baf433bcdfc5
David Johnson <djohnso08@gmail.com>
2022-06-25 14:06:45 -0500

    add prpoer syntax to if statement

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,21 @@ window.onload = function() {
     const quesThree = document.getElementById("question-three").value.toLowerCase();
     const quesFour = document.getElementById("question-four").value.toLowerCase();
     const quesFive = document.getElementById("question-five").value.toLowerCase();
+
+    let java = document.getElementById("java");
+    let questions = document.getElementById("questions");
+    let rust = document.getElementById("rust");
+
+    if (quesOne === "no" || quesFive === "yes") {
+      questions.style.display = "none";
+      java.style.display = "block";
+    }
+
+    else {
+      questions.style.display = "none";
+      rust.style.display = "block";
+    }
+
     event.preventDefault();
     console.log(quesOne);
   };
